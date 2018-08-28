fa0b22dae190f7cd751b12e69f7abc0f8cc08aa3
Bastien Thomas <bastien.thomas@happn.com>
2018-08-28 23:30:19 +0200

    Keeping up to date with kayak newest version

--- js/searchResult.js
+++ js/searchResult.js
@@ -1,58 +1,67 @@
+const weeks = 10;
+
 function save() {
+	const urlToJourneyRegex = /.*flights\/(.*)\/.*/g;
+	const journeyKey = urlToJourneyRegex.exec(window.location)[1];
 
-    var journeyKey = $("#origin").val() + " -> " + $("#destination").val();
+	const progress = 100;
 
-    var progress = 100
-    if ($(".ui-progressbar-value") !== null)
-        progress = Math.round($(".ui-progressbar-value").width() / $(".ui-progressbar-value").offsetParent().width() * 100);
+	// Wait until the page is loader
+	if ($('.Common-Results-ProgressBar .bar')[0].style.transform !== 'translateX(100%)' && $('[totalcolumns]').length == 0) {
+		if (progress == 100 || !Number.isInteger(progress)) {
+            chrome.storage.local.get('storedPrices', result => {
+            	if (result.storedPrices instanceof Object == false) {
+            		result.storedPrices = {};
+            	}
 
-    console.log("Waiting..." + progress + "%");
-    document.title = progress + "% - " + journeyKey;
+            	if (result.storedPrices[journeyKey] instanceof Object == false) {
+            		result.storedPrices[journeyKey] = {};
+            	}
 
-    if (progress == 100 || !Number.isInteger(progress)) {
-        chrome.storage.local.get('storedPrices', function(result) {
+                $('.Flights-Results-React-FlexMatrix-container [role="rowgroup"] li div:nth-child(4)').each(function () {
+                	const idToDateRegex = /.*__(\d\d\d\d)(\d\d)(\d\d)/g;
+                	const dates = idToDateRegex.exec($(this).closest('li').attr('id'));
 
-            if (result.storedPrices instanceof Object == false)
-                result.storedPrices = {};
+                	if (dates == null || !$.isNumeric($(this).html()[0])) {
+                		return;
+                	}
 
-            if (result.storedPrices[journeyKey] instanceof Object == false)
-                result.storedPrices[journeyKey] = {};
+                	const dateStr = dates[1] + '-' + dates[2] + '-' + dates[3];
 
-            $(".flexdatesmatrix .actionlink").each(function() {
-                var matches = $(this).attr("href").match(/javascript: FilterList\.flexFilterToDates\(\'([0-9]*)\', null\, ([0-9]*)\)/)
-                console.log(matches[1] + " : " + matches[2]);
+                    console.log('Kayak crawler - saving date ' + dateStr + ' price ' + $(this).html());
+                    result.storedPrices[journeyKey][dateStr] = $(this).html();
+                });
 
-                result.storedPrices[journeyKey][matches[1]] = matches[2];
+                console.log(result.storedPrices);
+
+                chrome.storage.local.set({
+                	storedPrices: result.storedPrices
+                }, () => {
+                    console.log('Kayak crawler - saved !');
+                    if (window.location.href.indexOf('autoclose') > -1) {
+                        window.close();
+                    }
+                });
             });
-
-            chrome.storage.local.set({
-                'storedPrices': result.storedPrices
-            }, function() {
-                console.log("saved");
-                if (window.location.href.indexOf("#autoclose") > -1) {
-                    window.close();
-                }
-            });
-        })
-    } else {
-        setTimeout(function() {
-            save();
-        }, 1000);
-    }
-
+		} else {
+            console.log("Kayak crawler - Waiting for page to be loaded");
+            setTimeout(() => {
+                save();
+            }, 1000);
+		}
+	}
 }
 
-$(".keel-grid.verticalsGrid").append('<div class="col-hidden col-vertical-l">' +
-    '<a id="crawl-link" style="color:red" >Crawl the next 10 weeks !</a></div>');
+$('.inline-search-form-grid').append('<div class="col-hidden col-vertical-l" style="width: 100%;text-align: right;">' +
+    '<a id="crawl-link" style="color: white; font-weight: bold">🐹 Crawl the next ' + weeks + ' weeks ! 🐹</a></div>');
 
-$("#crawl-link").on("click", function() {
-
-    var splitUrl = window.location.href.match(/(.*\/)([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])\.*/);
-    var currentDate = new Date(splitUrl[2]);
-    for (i = 0; i < 10; i++) {
-        var newDate = new Date(currentDate.setTime(currentDate.getTime() + 7 * 86400000));
-        window.open(splitUrl[1] + newDate.toISOString().substring(0, 10) + "-flexible#autoclose", '_blank');
-    }
+$('#crawl-link').on('click', () => {
+	const splitUrl = window.location.href.match(/(.*\/)(\d\d\d\d-\d\d-\d\d)\.*/);
+	const currentDate = new Date(splitUrl[2]);
+	for (i = 0; i < weeks; i++) {
+		const newDate = new Date(currentDate.setTime(currentDate.getTime() + 7 * 86400000));
+        window.open(splitUrl[1] + newDate.toISOString().substring(0, 10) + '-flexible?sort=bestflight_a&autoclose', '_blank');
+	}
 });
 
 save();
--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,30 @@
-$("button#reset").on("click", function() {
-    chrome.storage.local.clear(function() {
+$('button#reset').on('click', () => {
+    chrome.storage.local.clear(() => {
         alert('Voila !');
-    })
+    });
+});
+
+$('button#export').on('click', () => {
+    chrome.storage.local.get(['storedPrices'], result => {
+    	if (result.storedPrices instanceof Object == false) {
+    		result.storedPrices = {};
+    	}
+
+    console.log('getting storedPrices');
+
+    $.each(result.storedPrices, (journeyKey, value) => {
+        $('body').before(journeyKey + '<br/>');
+        let lower = 999999;
+        $.each(value, (date, price) => {
+        	if (price < price.replace( /^\D+/g, '')) {
+        		lower = price.replace( /^\D+/g, '');
+        	}
+            $('body').before(date + '-><span class=\'' + strhash(journeyKey) + price.replace( /^\D+/g, '') + '\'>' + price + '<br/>');
+        });
+        console.log(lower)
+        $('.' + strhash(journeyKey) + lower).css('color', 'red');
+        $('body').before('<br/>');
+    });
+    //  $("body").before(JSON.stringify(result, null, 2));
+    });
 });
